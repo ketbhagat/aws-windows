e80887d9f86c515c0b53f709e6c8822186646db7
Ketan Bhagat <88006325+ketbhagat@users.noreply.github.com>
2022-06-22 18:56:36 -0400

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,25 +4,35 @@ resource "aws_instance" "win" {
 	instance_type = "t2.micro"
 	vpc_security_group_ids = [ aws_security_group.allow_tls.id ]
 	user_data = <<EOF	
-<powershell>
-Set-Location "C:\Windows\system32"
+		<powershell>
+		Set-Location "C:\Windows\system32"
 
-#Change TimeZone
-C:\Windows\System32\tzutil /s "Indian Standard Time"
+		#Change TimeZone
+		C:\Windows\System32\tzutil /s "Indian Standard Time"
 
-#Install Chrome 
-$Path = $env:TEMP;
-$Installer = "chrome_installer.exe";
-Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile     $Path\$Installer; 
-Start-Process -FilePath $Path\$Installer -ArgumentList "/silent /install" -Verb RunAs -Wait;
-Remove-Item $Path\$Installer
+		#Install Chrome 
+		$Path = $env:TEMP;
+		$Installer = "chrome_installer.exe";
+		Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile     $Path\$Installer; 
+		Start-Process -FilePath $Path\$Installer -ArgumentList "/silent /install" -Verb RunAs -Wait;
+		Remove-Item $Path\$Installer
 
-#Set Chrome as default browser
-$chromePath = "${Env:ProgramFiles}\Google\Chrome\Application\" 
-$chromeApp = "chrome.exe"
-$chromeCommandArgs = "--make-default-browser"
-& "$chromePath$chromeApp" $chromeCommandArgs
-</powershell>
+		#Set Chrome as default browser
+		$chromePath = "${Env:ProgramFiles}\Google\Chrome\Application\" 
+		$chromeApp = "chrome.exe"
+		$chromeCommandArgs = "--make-default-browser"
+		& "$chromePath$chromeApp" $chromeCommandArgs
+
+		function Disable-InternetExplorerESC {
+		    $AdminKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}"
+		    $UserKey = "HKLM:\SOFTWARE\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}"
+		    Set-ItemProperty -Path $AdminKey -Name "IsInstalled" -Value 0
+		    Set-ItemProperty -Path $UserKey -Name "IsInstalled" -Value 0
+		    Stop-Process -Name Explorer
+		    Write-Host "IE Enhanced Security Configuration (ESC) has been disabled." -ForegroundColor Green
+		}
+		Disable-InternetExplorerESC
+		</powershell>
 	EOF
 	key_name = "Mumbai-KeyPair"
 	tags = {
