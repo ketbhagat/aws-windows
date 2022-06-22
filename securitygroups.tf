d624a56458fb6a14da80d584e1ba441234f3749b
Ketan Bhagat <88006325+ketbhagat@users.noreply.github.com>
2022-06-22 19:02:22 -0400

    Update securitygroups.tf

--- securitygroups.tf
+++ securitygroups.tf
@@ -2,7 +2,6 @@
 resource "aws_security_group" "allow_tls" {
 	name = "allow_tls"
 	description = "Allow TLS inbound traffic"
-	vpc_id = aws_vpc.main.id
 
 	dynamic "ingress" {
 		for_each = var.sg_windows
